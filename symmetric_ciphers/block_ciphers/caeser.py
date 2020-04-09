b8d098598137a6551316604e29e8ee0fe9993cb6
Dennis Gross <gdennis91@googlemail.com>
2020-04-10 01:22:59 +0200

    Add caesar cipher

---
+++ symmetric_ciphers/block_ciphers/caeser.py
@@ -0,0 +1,44 @@
+from enum import Enum
+from typing import List
+
+
+class Caesar:
+    _alphabet: List[str] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                            "s", "t", "u", "v", "w", "x", "y", "z"]
+
+    _alphabet_len = len(_alphabet)
+
+    class Direction(Enum):
+        LEFT = 1
+        RIGHT = 2
+
+    @classmethod
+    def encrypt(cls, plain_txt: str, shift: int):
+        return cls._routine(message=plain_txt, shift=shift, direction=cls.Direction.LEFT)
+
+    @classmethod
+    def decrypt(cls, cipher_txt: str, shift: int):
+        return cls._routine(message=cipher_txt, shift=shift, direction=cls.Direction.RIGHT)
+
+    @classmethod
+    def _routine(cls, message: str, shift: int, direction: Direction):
+        shifted_alphabet: List[str] = cls._shift_alphabet(shift=shift, direction=cls.Direction.LEFT)
+        plain_chars: List[str] = [char for char in message]
+
+        cipher_chars: List[str] = []
+        for char in plain_chars:
+            index = cls._alphabet.index(char)
+            cipher_chars.append(shifted_alphabet[index])
+
+        return "".join(cipher_chars)
+
+    @classmethod
+    def _shift_alphabet(cls, shift: int, direction: Direction):
+        if direction is cls.Direction.LEFT:
+            appendix: List[str] = cls._alphabet[0: shift]
+            suffix: List[str] = cls._alphabet[shift:len(cls._alphabet)]
+        else:
+            appendix: List[str] = cls._alphabet[0:(cls._alphabet_len - shift)]
+            suffix: List[str] = cls._alphabet[cls._alphabet_len - shift: cls._alphabet_len]
+
+        return suffix + appendix
---
+++ symmetric_ciphers/block_ciphers/test_caeser.py
@@ -0,0 +1,131 @@
+from random import seed, sample, randrange
+
+import pytest
+
+from symmetric_ciphers.block_ciphers.caeser import Caesar
+
+
+@pytest.mark.parametrize("params", [
+    {
+        "shift": 4,
+        "should": ["w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+                   "q", "r", "s", "t", "u", "v"],
+        "direction": Caesar.Direction.RIGHT
+    },
+    {
+        "shift": 6,
+        "should": ["u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+                   "o", "p", "q", "r", "s", "t"],
+        "direction": Caesar.Direction.RIGHT
+    },
+    {
+        "shift": 26,
+        "should": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                   "s", "t", "u", "v", "w", "x", "y", "z"],
+        "direction": Caesar.Direction.RIGHT
+    },
+    {
+        "shift": 52,
+        "should": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                   "s", "t", "u", "v", "w", "x", "y", "z"],
+        "direction": Caesar.Direction.RIGHT
+    },
+    {
+        "shift": 4,
+        "should": ["e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+                   "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d"],
+        "direction": Caesar.Direction.LEFT
+    },
+    {
+        "shift": 6,
+        "should": ["g", "h", "i", "j", "k", "l", "m", "n",
+                   "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f"],
+        "direction": Caesar.Direction.LEFT
+    },
+    {
+        "shift": 26,
+        "should": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                   "s", "t", "u", "v", "w", "x", "y", "z"],
+        "direction": Caesar.Direction.LEFT
+    },
+    {
+        "shift": 52,
+        "should": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                   "s", "t", "u", "v", "w", "x", "y", "z"],
+        "direction": Caesar.Direction.LEFT
+    },
+])
+def test_shift_alphabet(params):
+    assert Caesar._shift_alphabet(shift=params["shift"], direction=params["direction"]) == params["should"]
+
+
+@pytest.mark.parametrize("params", [
+    {
+        "shift": 4,
+        "plain": "",
+        "cipher": ""
+    },
+    {
+        "shift": 4,
+        "plain": "helloworld",
+        "cipher": "dahhksknhz"
+    },
+    {
+        "shift": 6,
+        "plain": "helloworld",
+        "cipher": "byffiqilfx"
+    },
+    {
+        "shift": 26,
+        "plain": "helloworld",
+        "cipher": "helloworld"
+    },
+])
+def test_decrypt(params):
+    assert Caesar.decrypt(cipher_txt=params["cipher"], shift=params["shift"]) == params["plain"]
+
+
+@pytest.mark.parametrize("params", [
+    {
+        "shift": 4,
+        "cipher": "",
+        "plain": ""
+    },
+    {
+        "shift": 4,
+        "cipher": "helloworld",
+        "plain": "dahhksknhz"
+    },
+    {
+        "shift": 6,
+        "cipher": "helloworld",
+        "plain": "byffiqilfx"
+    },
+    {
+        "shift": 26,
+        "cipher": "helloworld",
+        "plain": "helloworld"
+    },
+])
+def test_encrypt(params):
+    assert Caesar.encrypt(plain_txt=params["plain"], shift=params["shift"]) == params["cipher"]
+
+
+def _gen_payload():
+    seed(123)
+    payload = Caesar._alphabet
+    random_shift = randrange(start=0, stop=100000000, step=1)
+    random_txt = "".join(sample(payload, len(payload)))
+    return random_shift, random_txt
+
+
+@pytest.mark.repeat(1000)
+def test_cycle_random():
+    shift, txt = _gen_payload()
+    assert txt == Caesar.decrypt(
+        cipher_txt=Caesar.encrypt(
+            plain_txt=txt,
+            shift=shift
+        ),
+        shift=shift
+    )
